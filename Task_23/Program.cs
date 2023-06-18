ce18284e8450c4998b59afb857daa728b382e1ff
Daniil <oleinik.d_78@mail.ru>
2023-06-18 14:25:42 +0400

    DZ Task 23

--- Task_23/Program.cs
+++ Task_23/Program.cs
@@ -2,3 +2,12 @@
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+
+System.Console.Write("Вывести все кубы чисел от 1 до ");
+int NumberOfCube = Convert.ToInt32(Console.ReadLine());
+
+for (int i=1; i<=NumberOfCube; i++)
+{
+    System.Console.WriteLine($"{Math.Pow(i, 3)}");
+}
